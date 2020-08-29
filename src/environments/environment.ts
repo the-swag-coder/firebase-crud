c8a2c043393d30235d175f9a17bb6e83fe49c621
Swagat Suman <swagat@sulopa.in>
2020-08-29 13:04:26 +0530

    Environment variable changes.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
 
   firebaseConfig: {
-    apiKey: 'AIzaSyB8NsGToBSi6OyVq6vls0-bDJ1fKmB6M8Y',
-    authDomain: 'fir-crud-swag-coder.firebaseapp.com',
-    databaseURL: 'https://fir-crud-swag-coder.firebaseio.com',
-    projectId: 'fir-crud-swag-coder',
-    storageBucket: 'fir-crud-swag-coder.appspot.com',
-    messagingSenderId: '666123075763',
-    appId: '1:666123075763:web:3714d461ba2f81afb6b0c6',
-    measurementId: 'G-PRC33ZP1DN'
+    apiKey: '<Firebase API key>',
+    authDomain: '<Firebase auth domain>',
+    databaseURL: '<Firebase database URL>',
+    projectId: '<Firebase project ID>',
+    storageBucket: '<Firebase storage bucket>',
+    messagingSenderId: '<Firebase messaging sender ID>',
+    appId: '<Firebase app ID>',
+    measurementId: '<Firebase measurement ID>'
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
